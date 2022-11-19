deaa26c4593f77720ade0a6179162cb281fcdf9a
SamZhang02 <sam.zijing.zhang@gmail.com>
2022-11-19 17:10:31 -0500

    update index cjs

--- index.cjs
+++ index.cjs
@@ -1,17 +1,18 @@
-const http = require('http');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const expres = require('express');
+const express = require('express');
+const app = express();
+const PORT = 8000;
 
-const hostname = '127.0.0.1';
-const port = 8000;
+app.use(express.static('.'))
+app.use(express.json())
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.post('/', (req,res) => {
+  const {parcel} = req.body;
+  if (!parcel) {
+    return res.status(400).send({status: 'failed'});
+  }
+  res.status(200).send({status: 'received'})
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server connected at http://${hostname}:${port}/`);
-});
+app.listen(PORT, () => {
+  console.log(`Server connected at port ${PORT}`);
+})

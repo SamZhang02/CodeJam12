586d0d46ea82961d290a783f0d7400aef2a150f2
unknown <01simon.wu@gmail.com>
2022-11-19 15:08:31 -0500

    basic event listener to store input data

--- frontend/trip_info/tripinfo.js
+++ frontend/trip_info/tripinfo.js
@@ -1,3 +1,26 @@
-const tripInfo = document.getElementById("trip-info").submit()
+const tripInfo = document.getElementById('trip-info')
 
 console.log(tripInfo)
+
+const inputs = document.querySelectorAll('.required-input')
+
+tripInfo.addEventListener('submit', () => {
+  
+  event.preventDefault();
+
+  const dataArray = []
+
+  inputs.forEach(input => {
+
+    const inputObject = {}
+
+    inputObject.name = input.id
+
+    inputObject.value = input.value
+
+    dataArray.push(inputObject)
+    
+  });
+
+  console.log(dataArray)
+})
